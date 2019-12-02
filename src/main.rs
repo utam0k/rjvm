a29b2aac0ab81d26af8eb1cbb1a7fab06090c0f1
utam0k <k0ma@utam0k.jp>
2019-12-03 01:18:06 +0900

    read a magic number.

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,14 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let file_path = "./samples/HelloWorld.class";
     let mut f = File::open(&file_path)?;
     let mut buf = Vec::new();
-    let magic: &mut [u8] = &mut [0; 8];
+    let magic: &mut [u8] = &mut [0; 4];
 
     let _ = f.read_to_end(&mut buf)?;
     let mut cur = Cursor::new(buf);
     cur.read_exact(magic)?;
-    dbg!(magic);
+    for b in magic {
+        print!("{:x}", b);
+    }
+    println!();
     Ok(())
 }
