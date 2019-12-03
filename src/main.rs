e00599fa30d9bdd19a9e04f232f03069bd01799f
utam0k <k0ma@utam0k.jp>
2019-12-04 01:13:11 +0900

    parse version.

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,46 @@
+use std::fmt;
 use std::fs::File;
-use std::io::Cursor;
 use std::io::Read;
+use std::mem::{size_of, transmute};
+
+#[repr(C)]
+struct Class {
+    magic: [u8; 4],
+    minor_version: [u8; 2],
+    major_version: [u8; 2],
+}
+
+impl fmt::Display for Class {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "magic: ")?;
+        for b in self.magic.iter() {
+            write!(f, "{:x}", b)?;
+        }
+        writeln!(f)?;
+        write!(f, "minor_version: ")?;
+        for b in self.minor_version.iter() {
+            write!(f, "{}", b)?;
+        }
+        writeln!(f)?;
+        write!(f, "major_version: ")?;
+        for b in self.major_version.iter() {
+            write!(f, "{}", b)?;
+        }
+        Ok(())
+    }
+}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let file_path = "./samples/HelloWorld.class";
-    let mut f = File::open(&file_path)?;
-    let mut buf = Vec::new();
-    let magic: &mut [u8] = &mut [0; 4];
+    let mut file = File::open(file_path)?;
 
-    let _ = f.read_to_end(&mut buf)?;
-    let mut cur = Cursor::new(buf);
-    cur.read_exact(magic)?;
-    for b in magic {
-        print!("{:x}", b);
-    }
-    println!();
+    let class: Class = {
+        let mut c = [0u8; size_of::<Class>()];
+        file.read_exact(&mut c[..])?;
+        unsafe { transmute(c) }
+    };
+
+    println!("{}", class);
+
     Ok(())
 }
